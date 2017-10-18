9dafbb87ab87df91eb02e7f27ac6e42b4abad6f3
Alfredo Ortega <ortegaalfredo@gmail.com>
2017-10-18 16:40:57 -0300

    Added little ascii graph output

--- hdd-killer/hdd-killer.py
+++ hdd-killer/hdd-killer.py
@@ -1,7 +1,7 @@
-"""PyAudio hdd-killer: Generate sound and interfere with HDD """
+"""PyAudio hdd-killer: Generate sound and interfere with a spinning HDD (not SSDs) """
 """Alfredo Ortega @ortegaalfredo"""
-"""Usage: hdd-killer /dev/sdX"""
-"""Where /dev/sdX is a spinning hard-disk drive"""
+"""Usage: hdd-killer <file>"""
+"""Where <file> is any big file. Can be a /dev/sdX raw device, it will be opened read-only."""
 """Turn the volume to the max for better results"""
 """Requires: pyaudio. Install with 'sudo pip install pyaudio' or 'sudo apt-get install python-pyaudio'"""
 
@@ -17,9 +17,9 @@ FREQ=50
 # validation. If a disk hasn't been specified, exit.
 if len(sys.argv) < 2:
     print "hdd-killer: Attempt to interfere with a hard disk, using sound.\n\n" +\
-	  "The disk will be opened as read-only.\n" + \
+	  "The file will be opened as read-only.\n" + \
           "Warning: It might cause damage to HDD.\n" +\
-          "Usage: %s /dev/sdX" % sys.argv[0]
+          "Usage: %s <file> (can also be a raw device ex. /dev/sdX)" % sys.argv[0]
     sys.exit(-1)
 
 # instantiate PyAudio (1)
@@ -34,10 +34,13 @@ def callback(in_data, frame_count, time_info, status):
     sample=0
     for x in xrange(frame_count):
         oldsample=sample
-        sample=chr(int(math.sin(x1*((2*math.pi)/(RATE/FREQ)))*127)+128)
-        data = data+sample
+        sample=int(math.sin(x1*((2*math.pi)/(RATE/FREQ)))*127)+128
+#### uncomment for square wave
+#        if sample<128: sample=10
+#        if sample>128: sample=250
+        data = data+chr(sample)
         # continous frequency change
-        if (NEWFREQ!=FREQ) and (sample==chr(128)) and (oldsample<sample) :
+        if (NEWFREQ!=FREQ) and (sample==128) and (oldsample<sample) :
                 FREQ=NEWFREQ
                 x1=0
         x1+=1
@@ -54,19 +57,25 @@ stream = p.open(format=pyaudio.paUInt8,
 stream.start_stream()
 
 # wait for stream to finish (5)
+div=0
+divcnt=0
 while stream.is_active():
     timeprom=0
     c=file(sys.argv[1])
-    for i in xrange(20):
+    for i in xrange(30):
         a=time.clock()
         c.seek(random.randint(0,1000000000),1) #attempt to bypass file buffer
         c.read(51200)
         b=time.clock()
         timeprom+=b-a
     c.close()
-    timeprom/=20
-    print("Frequency: %.2f Hz File Read prom: %f us" % (FREQ,timeprom*1000000))
-    NEWFREQ+=0.5
+    timeprom/=30
+    if (divcnt<5):
+        divcnt+=1
+        div+=timeprom
+    else:
+        print("%.2f Hz Delay: %f us %s" % (FREQ,timeprom*1000000,"="*int(timeprom/(div/50))))
+    NEWFREQ+=0.25
 
 # stop stream (6)
 stream.stop_stream()
